7d4cc73384e6400f25b09f1c378216e556a35d85
clayton <clayton.beraldi@jacotei.com.br>
2016-01-26 18:43:56 -0200

    tipos de clientes - iniciando

--- classes/Cliente.php
+++ classes/Cliente.php
@@ -6,14 +6,113 @@ class Cliente {
     public $idade;
     public $sexo;
     public $endereco;
+    public $pessoa;
 
-    public function __construct($nome,$cpf,$idade,$sexo,$endereco){
+    public function __construct($nome,$cpf,$idade,$sexo,$endereco,$pessoa){
         $this->nome = $nome;
         $this->cpf = $cpf;
         $this->idade = $idade;
         $this->sexo = $sexo;
         $this->endereco = $endereco;
+        $this->pessoa = $pessoa;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * @param mixed $idade
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    /**
+     * @param mixed $sexo
+     */
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * @param mixed $endereco
+     */
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPessoa()
+    {
+        return $this->pessoa;
+    }
+
+    /**
+     * @param mixed $pessoa
+     */
+    public function setPessoa($pessoa)
+    {
+        $this->pessoa = $pessoa;
+    }
+
 }
 
 
--- index.php
+++ index.php
@@ -12,16 +12,16 @@ function filtrar($var)
 }
 
 $cliente = array(
-    0 => new Cliente('Clayton Lima','111111111111','30','M','Rua perdida SP'),
-    1 => new Cliente('Carol Lima','22222222222','45','F','Rua achei SP'),
-    2 => new Cliente('Geo Lima','33333333','55','F','Rua perdida SP'),
-    3 => new Cliente('Bruno Lima','44444444','18','M','Rua perdida SP'),
-    4 => new Cliente('Lucas Lima','5555555555','14','M','Rua perdida SP'),
-    5 => new Cliente('Luan Lima','6666666','30','M','Rua perdida SP'),
-    6 => new Cliente('Fernando Lima','9999999','25','M','Rua perdida SP'),
-    7 => new Cliente('Rodrigo Lima','4101010101','20','M','Rua perdida SP'),
-    8 => new Cliente('Paula Lima','121212121212','28','M','Rua perdida SP'),
-    9 => new Cliente('Eduardo Lima','131313131331','55','M','Rua perdida SP')
+    0 => new Cliente('Clayton Lima','111111111111','30','M','Rua perdida SP','PJ'),
+    1 => new Cliente('Carol Lima','22222222222','45','F','Rua achei SP','PF'),
+    2 => new Cliente('Geo Lima','33333333','55','F','Rua perdida SP','PJ'),
+    3 => new Cliente('Bruno Lima','44444444','18','M','Rua perdida SP','PF'),
+    4 => new Cliente('Lucas Lima','5555555555','14','M','Rua perdida SP','PF'),
+    5 => new Cliente('Luan Lima','6666666','30','M','Rua perdida SP','PJ'),
+    6 => new Cliente('Fernando Lima','9999999','25','M','Rua perdida SP','PF'),
+    7 => new Cliente('Rodrigo Lima','4101010101','20','M','Rua perdida SP','PJ'),
+    8 => new Cliente('Paula Lima','121212121212','28','M','Rua perdida SP','PJ'),
+    9 => new Cliente('Eduardo Lima','131313131331','55','M','Rua perdida SP','PF')
 );
 
 if(@$_GET['ordenar']== 1){
@@ -68,9 +68,22 @@ else
             <?php endforeach; ?>
         </select>
 
-        <input type="radio" name="ordenar" value="0">Ascendente<br>
-        <input type="radio" name="ordenar" value="1" style="margin-bottom: 10px;">Descendente<br>
-        <button class="btn btn-info col-md-5 col-md-offset-1" style="width:225px; margin-left: 135px; margin-bottom: 20px"type="submit">Consultar</button>
+
+     <div>
+         <fieldset>
+            <input type="radio" name="ordenar" value="0">Ascendente<br>
+            <input type="radio" name="ordenar" value="1" style="margin-bottom: 10px;">Descendente<br>
+         </fieldset>
+     </div>
+
+     <div>
+         <fieldset>
+             <input type="radio" name="ordenar" value="0">Pessoa Física<br>
+             <input type="radio" name="ordenar" value="1" style="margin-bottom: 10px;">Pessoa Jurídica<br>
+         </fieldset>
+     </div>
+
+         <button class="btn btn-info col-md-5 col-md-offset-1" style="width:225px; margin-left: 135px; margin-bottom: 20px"type="submit">Consultar</button>
     </form>
 
     <?php if(!empty($filtro)):?>
@@ -80,6 +93,7 @@ else
             <th>#</th>
             <th>Nome</th>
             <th>CPF</th>
+            <th>Pessoa</th>
             <th>Idade</th>
             <th>Sexo</th>
             <th>Endereço</th>
@@ -95,6 +109,7 @@ else
                 <td><?php echo $row_count++; ?></td>
                 <td><?php echo $clientes->nome ; ?></td>
                 <td><?php echo $clientes->cpf; ?></td>
+                <td><?php echo $clientes->pessoa; ?></td>
                 <td><?php echo $clientes->idade; ?></td>
                 <td><?php echo $clientes->sexo; ?></td>
                 <td><?php echo $clientes->endereco; ?></td>
