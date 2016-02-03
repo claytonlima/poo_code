a7ba4368d804483f526752fb9f5918cf511adf31
clayton <clayton.beraldi@jacotei.com.br>
2016-02-03 14:36:26 -0200

    add composer

--- classes/Cliente.php
+++ classes/Cliente.php
@@ -1,6 +1,8 @@
 <?php
+require_once __DIR__ . "../interfaces/EnderecoCobranca.php";
+require_once __DIR__ . "../interfaces/GrauImportancia.php";
 
-class Cliente {
+class Cliente implements EnderecoCobranca,GrauImportancia {
     public $nome;
     public $cpf;
     public $idade;
@@ -8,8 +10,10 @@ class Cliente {
     public $endereco;
     public $pessoa;
     public $estrela;
+    public $EnderecoCobranca;
+    public $GrauImportancia;
 
-    public function __construct($nome,$cpf,$idade,$estrela,$sexo,$endereco,$pessoa){
+    public function __construct($nome = null,$cpf = null ,$idade = null,$estrela = null,$sexo = null,$endereco = null,$pessoa = null, $EnderecoCobranca = null, $GrauImportancia = null){
         $this->nome = $nome;
         $this->cpf = $cpf;
         $this->idade = $idade;
@@ -17,128 +21,109 @@ class Cliente {
         $this->sexo = $sexo;
         $this->endereco = $endereco;
         $this->pessoa = $pessoa;
+        $this->pessoa = $EnderecoCobranca;
+        $this->pessoa = $GrauImportancia;
 
     }
 
-    /**
-     * @return mixed
-     */
     public function getEstrela()
     {
         return $this->estrela;
     }
 
-    /**
-     * @param mixed $estrela
-     */
     public function setEstrela($estrela)
     {
         $this->estrela = $estrela;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getNome()
     {
         return $this->nome;
     }
 
-    /**
-     * @param mixed $nome
-     */
     public function setNome($nome)
     {
         $this->nome = $nome;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getCpf()
     {
         return $this->cpf;
     }
 
-    /**
-     * @param mixed $cpf
-     */
     public function setCpf($cpf)
     {
         $this->cpf = $cpf;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getIdade()
     {
         return $this->idade;
     }
 
-    /**
-     * @param mixed $idade
-     */
     public function setIdade($idade)
     {
         $this->idade = $idade;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getSexo()
     {
         return $this->sexo;
     }
 
-    /**
-     * @param mixed $sexo
-     */
     public function setSexo($sexo)
     {
         $this->sexo = $sexo;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getEndereco()
     {
         return $this->endereco;
     }
 
-    /**
-     * @param mixed $endereco
-     */
     public function setEndereco($endereco)
     {
         $this->endereco = $endereco;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getPessoa()
     {
         return $this->pessoa;
     }
 
-    /**
-     * @param mixed $pessoa
-     */
     public function setPessoa($pessoa)
     {
         $this->pessoa = $pessoa;
         return $this;
     }
 
+    public function getEnderecoCobranca()
+    {
+        return $this->EnderecoCobranca;
+    }
+
+    public function setEnderecoCobranca($EnderecoCobranca)
+    {
+        $this->EnderecoCobranca = $EnderecoCobranca;
+        return $this;
+    }
+
+    public function getGrauImportancia()
+    {
+        return $this->GrauImportancia;
+    }
+
+    public function setGrauImportancia($GrauImportancia)
+    {
+        $this->GrauImportancia = $GrauImportancia;
+        return $this;
+    }
 }
 
 
